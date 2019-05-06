1ee1ca298b32309cd67f99a3de58379104941b0d
Tomasz Mloduchowski <q@qdot.me>
2019-05-06 18:11:23 -0500

    Export StartTime as well

--- raw2pickle.py
+++ raw2pickle.py
@@ -10,7 +10,9 @@ rval = []
 for i in range(r.GetFirstSpectrumNumber(),r.GetLastSpectrumNumber()):
     if r.GetMSOrderForScanNum(i) == 1:
         rl = r.GetLabelData(i)
-        rval.append(numpy.array([rl[0][0],rl[0][1]])) 
+        rval.append((
+            r.GetScanHeaderInfoForScanNum(i)['StartTime'],
+            numpy.array([rl[0][0],rl[0][1]]))) 
         
 print(len(rval))
 pickle.dump( rval, open(os.path.splitext(sys.argv[1])[0]+".spectra.p", "wb" ) )
